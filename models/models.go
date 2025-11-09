26a34eb7371da46f2398df0a5382026e88adcb62
Owain Stringer <owain.stringer@capventis.com>
2025-11-09 18:44:47 +0000

    updated data

--- models/models.go
+++ models/models.go
@@ -1,14 +1,14 @@
 package models
 
 type EC2Instance struct {
-	InstanceID    string  `json:"InstanceId"`
-	Name          string  `json:"Name"`
-	InstanceType  string  `json:"Type"`
-	PublicIP      string  `json:"PublicIP"`
-	PrivateIP     string  `json:"PrivateIP"`
-	State         string  `json:"State"`
-	OSInfo        string  `json:"OSInfo"`        
-	SuggestedType string  `json:"SuggestedType"` 
-	Cores         *int    `json:"Cores,omitempty"`
-	Threads       *int    `json:"Threads,omitempty"`
+	Name        string `json:"Name"`
+	InstanceID  string `json:"InstanceId"`
+	State       string `json:"State"`
+	Type        string `json:"Type"`
+	AZ          string `json:"AZ"`
+	PrivateIP   string `json:"PrivateIP"`
+	Project     string `json:"Project"`
+	Tenant      string `json:"Tenant"`
+	Owner       string `json:"Owner"`
 }
+
